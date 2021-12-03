2d79e22bce40bdea24ff810d12dcd04a614b5ec1
DanWilk <dcwilkinson@wilktech.com>
2021-12-02 20:02:03 -0500

    added event model

---
+++ models/Events.js
@@ -0,0 +1,51 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Events extends Model {
+
+}
+
+Events.init(
+   {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        event_name: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        event_month: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        event_day: {
+            type: Datatypes.INTEGER,
+            allowNull: false
+        },
+        event_year: {
+            type: Datatypes.INTEGER,
+            allowNull: false
+        },
+        event_location: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        event_bands: {
+            reference: {
+                model: 'Band',
+                key: 'id'
+            }
+        },
+        event_genres: {
+            references: {
+                model: 'genre',
+                key: 'id'
+            }
+        }
+    }
+)
+
+module.exports = Events;
