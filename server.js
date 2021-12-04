a08ff6f448fcdc70e0254f30d445d91a08346dc6
ZBreezie <zpmc55@yahoo.com>
2021-12-04 16:37:41 -0500

    added main handlebars layout, and possible corresponding pages

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 // const routes = require('./routes');
 const sequelize = require('./config/connection');
-// const expbs = require('express-handlebars')
+const { engine } = require("express-handlebars");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.engine('handlebars', expbs());
-// app.set('view engine', 'handlebars');
+
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +18,25 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // app.use(routes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+//   });
+
+app.get('/', (req, res) =>{
+  res.render('home');
+});
+
+app.get('/signup', (req, res) =>{
+  res.render('signup');
+});
+
+app.get('/events', (req, res) =>{
+  res.render('events');
+});
+
+app.get('/genres', (req, res) =>{
+  res.render('genres');
+});
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
