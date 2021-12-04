c225a211a67990d670243b481b75dffcbcc40c8e
Will B <tryarc@comcast.net>
2021-12-04 12:03:16 -0500

    added heroku fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
