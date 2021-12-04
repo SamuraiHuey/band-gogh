a96526a1571be08bf615f5f3760e78f080ed5c80
ailict <ailichatfieldtaylor@gmail.com>
2021-12-04 12:53:53 -0500

    updated band seeds to account for genre update in model js

--- seeds/seeds-band.js
+++ seeds/seeds-band.js
@@ -6,6 +6,7 @@ const logins = [
     id: '001',
     band_name: 'Gwar',
     band_website: 'https://gwar.net/',
+    genre_name: 'Metal',
     username: 'gwar1984',
     email: 'gwar@gwarbar.com',
     password: 'password123',
@@ -14,6 +15,7 @@ const logins = [
     id: '002',
     band_name: 'Carbon Leaf',
     band_website: 'http://www.carbonleaf.com/',
+    genre_name: 'Rock',
     username: 'carbon1992',
     email: 'carbon@leaf.com',
     password: 'password123'
@@ -22,6 +24,7 @@ const logins = [
     id: '003',
     band_name: 'Pat McGee',
     band_website: 'patmcgee.net',
+    genre_name: 'Country',
     username: 'mcgee1996',
     email: 'pat@mcgee.com',
     password: 'password123'
@@ -30,6 +33,7 @@ const logins = [
     id: '004',
     band_name: 'River City High',
     band_website: 'https://en.wikipedia.org/wiki/River_City_High',
+    genre_name: 'Rock',
     username: 'rch1999',
     email: 'rivercity@high.com',
     password: 'password123'
@@ -38,6 +42,7 @@ const logins = [
     id: '005',
     band_name: 'Cannabis Corpse',
     band_website: 'https://cannabiscorpseofficial.bandcamp.com',
+    genre_name: 'Punk',
     username: 'cannabis1996',
     email: 'cannabis@corpse.com',
     password: 'password123'
@@ -46,6 +51,7 @@ const logins = [
     id: '006',
     band_name: 'Sparklehorse',
     band_website: 'https://www.marklinkous.com/',
+    genre_name: 'Rock',
     username: 'sparklehorse1995',
     email: 'mark@sparkle.com',
     password: 'password123'
@@ -54,6 +60,7 @@ const logins = [
     id: '007',
     band_name: 'Four Walls Falling',
     band_website: 'https://fourwallsfalling413.bandcamp.com/album/four-walls-falling',
+    genre_name: 'Punk',
     username: 'fwf1983',
     email: 'fourwalls@falling.com',
     password: 'password123'
@@ -62,6 +69,7 @@ const logins = [
     id: '008',
     band_name: 'Alabama Thunderpussy',
     band_website: 'https://alabamathunderpussyband.bandcamp.com/album/open-fire',
+    genre_name: 'Metal',
     username: 'thunderpussy1996',
     email: 'alabama@thunderpussy.com',
     password: 'password123'
@@ -70,6 +78,7 @@ const logins = [
     id: '009',
     band_name: 'Down to Nothing',
     band_website: 'https://downtonothing.bandcamp.com/album/live-on-the-james',
+    genre_name: 'Punk',
     username: 'dtn2000',
     email: 'downto@nothing.com',
     password: 'password123'
@@ -78,6 +87,7 @@ const logins = [
     id: '010',
     band_name: 'The Virginians',
     band_website: 'https://www.virginians.org/',
+    genre_name: 'Country',
     username: 'va1952',
     email: 'the@virginians.com',
     password: 'password123'
