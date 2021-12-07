f82b21457a64cb690139d0f7175f3ee04a126d3c
ZBreezie <zpmc55@yahoo.com>
2021-12-07 16:53:40 -0500

    basic express-session install

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const { engine } = require("express-handlebars");
 
+// const session = require('express-session');
+
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
