5fe1daa470bdddc958e8798a2e7e4119e348d81d
ZBreezie <zpmc55@yahoo.com>
2021-12-11 12:29:07 -0500

    reroute to post page on login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/post');
     } else {
       alert(response.statusText);
     }
